ce3e9d24dd36b4b22f5a7986be3b365a33a48434
kjessup31 <kjessup@paprep.org>
2019-12-16 22:02:21 +0000

    deleted input for number of derivatives

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ x = Symbol('x')
 #ogeq means original equation
 ogeq = eval(input("enter equation: f =  ")) #need to change eval. input does not work
 f = ogeq
-#user enters how many derivtives of the equations
-#diffnumb means the derivatie number
-diffnumb = int(input("enter the number of diffs wanted (up to 5): "))
+for i in range(5):
+  f
