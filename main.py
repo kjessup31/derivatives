258ff5e05d02a6a34125242392a1ef62d124964d
kjessup31 <kjessup@paprep.org>
2019-12-16 22:56:50 +0000

    unsuccessful lambda attempt

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import sympy
+"""import sympy
 from sympy import diff
 from sympy import Symbol
-x = Symbol('x')
-#user enters equation
-#ogeq means original equation
-ogeq = eval(input("enter equation: f =  ")) #need to change eval. input does not work
+from sympy import var
+from sympy.utilities.lambdify import lambdify
+f = lambdify(x, expr)
+x = var('x')
+ogeq = input("enter equation: f =  ")
 f = ogeq
-#user enters how many derivtives of the equations
-#diffnumb means the derivatie number
 diffnumb = int(input("enter the number of diffs wanted: "))
 for i in range(diffnumb):
   f = f.diff(x)
-  print(f)
+  print(f)"""
+import sympy
+from sympy import var
+from sympy import sympify
+from sympy.utilities.lambdify import lambdify
+f = input("")
+x = var('x')
+expr = f.diff(x)
+r = lambdify(x, expr)
+print(r)
