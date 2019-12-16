b27baaefa911263c76fddd2d9c185a8d2077fd60
kjessup31 <kjessup@paprep.org>
2019-12-16 22:14:06 +0000

    modified the if then statements to a simple for loop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sympy
 from sympy import diff
-fr
+from sympy import Symbol
 x = Symbol('x')
 #user enters equation
 #ogeq means original equation
@@ -8,51 +8,7 @@ ogeq = eval(input("enter equation: f =  ")) #need to change eval. input does not
 f = ogeq
 #user enters how many derivtives of the equations
 #diffnumb means the derivatie number
-diffnumb = int(input("enter the number of diffs wanted (up to 5): "))
-if diffnumb == 1:
-  f_prime = f.diff(x)
-  print(f_prime)
-elif diffnumb == 2:
-  f_prime = f.diff(x)
-  print(f_prime)
-  f2prime = f_prime
-  f_2prime = f2prime.diff(x)
-  print(f_2prime)
-elif diffnumb == 3:
-  f_prime = f.diff(x)
-  print(f_prime)
-  f2prime = f_prime
-  f_2prime = f2prime.diff(x)
-  print(f_2prime)
-  f3prime = f_2prime
-  f_3prime = f3prime.diff(x)
-  print(f_3prime)
-elif diffnumb == 4:
-  f_prime = f.diff(x)
-  print(f_prime)
-  f2prime = f_prime
-  f_2prime = f2prime.diff(x)
-  print(f_2prime)
-  f3prime = f_2prime
-  f_3prime = f3prime.diff(x)
-  print(f_3prime)
-  f4prime = f_3prime
-  f_4prime = f4prime.diff(x)
-  print(f_4prime)
-elif diffnumb == 5:
-  f_prime = f.diff(x)
-  print(f_prime)
-  f2prime = f_prime
-  f_2prime = f2prime.diff(x)
-  print(f_2prime)
-  f3prime = f_2prime
-  f_3prime = f3prime.diff(x)
-  print(f_3prime)
-  f4prime = f_3prime
-  f_4prime = f4prime.diff(x)
-  print(f_4prime)
-  f5prime = f_4prime
-  f_5prime = f5prime.diff(x)
-  print(f_5prime)
-else:
-  print("Can only differentiate up to five times")
+diffnumb = int(input("enter the number of diffs wanted: "))
+for i in range(diffnumb):
+  f = f.diff(x)
+  print(f)
